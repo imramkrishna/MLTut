0222ccaf4a0ae1c3421ae08d4ba6a8574db85c33
Ram Krishna Yadav <itsramky234@gmail.com>
2025-09-16 08:07:29 +0545

    added pandas and numpy csv for data analysis

---
+++ pandas/test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data={
+    'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+    'Age': [24, 27, 22, 32],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
+}
+
+df = pd.DataFrame(data)
+print(df)
+print(df.shape)
+print(df.ndim)
+print(df.loc[[0,1]])
---
+++ pandas/test2.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data=pd.read_csv("pandas/employees.csv")
+data.plot()
+plt.show()
